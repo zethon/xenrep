31cf02f85146458fe97b7ddd34a5239904046fba
Addy <aclaure@gmail.com>
2020-11-06 08:00:21 -0500

    add rep table;

--- Setup.php
+++ Setup.php
@@ -7,9 +7,27 @@ use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
 use XF\AddOn\StepRunnerUpgradeTrait;
 
+use XF\Db\Schema\Alter;
+use XF\Db\Schema\Create;
+
 class Setup extends AbstractSetup
 {
 	use StepRunnerInstallTrait;
 	use StepRunnerUpgradeTrait;
-	use StepRunnerUninstallTrait;
+    use StepRunnerUninstallTrait;
+    
+    public function installStep1()
+    {
+        $this->schemaManager()->createTable('lulzapps_reputation', 
+            function(Create $table)
+            {
+                $table->addColumn('repid', 'int');
+                $table->addColumn('post_id', 'int');
+                $table->addColumn('user_id', 'int');
+                $table->addColumn('reputation', 'int');
+                $table->addColumn('comment', 'varchar', 255)->setDefault('');
+                $table->addColumn('date', 'int');
+                $table->addPrimaryKey('repid');
+            });
+    }
 }
--- XF/Pub/Controller/Post.php
+++ XF/Pub/Controller/Post.php
@@ -18,26 +18,13 @@ class Post extends XFCP_Post
         return $this->message('Hello world!111');
     }
 
-	// public function actionShow(ParameterBag $params)
-	// {
-	// 	$post = $this->assertViewablePost($params->post_id);
-
-	// 	$viewParams = [
-	// 		'post' => $post,
-	// 		'thread' => $post->Thread,
-	// 		'forum' => $post->Thread->Forum,
-	// 		'canInlineMod' => $post->canUseInlineModeration()
-	// 	];
-	// 	return $this->view('XF:Post\Show2', 'post', $viewParams);
-	// }
-
     public function actionReputation(ParameterBag $params) 
     {
         $viewParams = 
         [
             'post_id' => $params->post_id,
-            // 'params' => print_r($params->params())
         ];
+        
         return $this->view('lulzapps:RepView', 'lulzapps_reputation_submit_overlay', $viewParams);
     }
 }
