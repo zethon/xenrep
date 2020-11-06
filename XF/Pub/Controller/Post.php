d3f36371fb15b24d34c443404bf68ea985056fee
Addy <aclaure@gmail.com>
2020-11-05 21:18:43 -0500

    this is something;

--- XF/Pub/Controller/Post.php
+++ XF/Pub/Controller/Post.php
@@ -1,12 +1,38 @@
 <?php
 
-namespace lulzapps\Rep\XF\Entity;
+namespace lulzapps\Rep\XF\Pub\Controller;
+
+use XF\Mvc\ParameterBag;
 
 class Post extends XFCP_Post
 {
+    public function actionEdit(ParameterBag $params)
+    {
+        $reply = parent::actionEdit($params);
+        echo print_r($reply);
+        return $reply;
+    }
+
+    public function actionHelloWorld()
+    {
+        return $this->message('Hello world!111');
+    }
+
+	// public function actionShow(ParameterBag $params)
+	// {
+	// 	$post = $this->assertViewablePost($params->post_id);
+
+	// 	$viewParams = [
+	// 		'post' => $post,
+	// 		'thread' => $post->Thread,
+	// 		'forum' => $post->Thread->Forum,
+	// 		'canInlineMod' => $post->canUseInlineModeration()
+	// 	];
+	// 	return $this->view('XF:Post\Show2', 'post', $viewParams);
+	// }
+
     public function actionReputation(ParameterBag $params) 
     {
-        echo ("HI THERE");
-        echo print_r($params, true);
+        return $this->view('lulzapps:RepView', 'lulzapps_reputation_submit_overlay', []);
     }
 }
---
+++ _output/extension_hint.php
@@ -0,0 +1,9 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace lulzapps\Rep\XF\Pub\Controller
+{
+	class XFCP_Post extends \XF\Pub\Controller\Post {}
+}
