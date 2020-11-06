ae68ef0502d49af7af0e46fd545b10d03a4cfc8b
Addy <aclaure@gmail.com>
2020-11-06 16:43:50 -0500

    no message

---
+++ Listener.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace lulzapps\Rep;
+
+use XF\Mvc\Entity\Entity;
+
+class Listener
+{
+    public static function postEntityStructure(\XF\Mvc\Entity\Manager $em, \XF\Mvc\Entity\Structure &$structure)
+    {
+
+    }
+}
