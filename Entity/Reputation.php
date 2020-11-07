f30705a1c5a888eb85a7fe89d25ce037be0c3135
Addy <aclaure@gmail.com>
2020-11-06 21:54:35 -0500

    more work;

---
+++ Entity/Reputation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Demo\Portal\Entity;
+
+use XF\Mvc\Entity\Structure;
+
+class Reputation extends \XF\Mvc\Entity\Entity
+{
+    public static function getStructure(Structure $structure)
+    {
+        $structure->table = 'lulzapps_reputation';
+        $structure->shortName = 'lulzapps\Rep:Reputation';
+        $structure->primaryKey = 'rep_id';
+        $structure->columns = 
+            [
+                'rep_id' => ['type' => self::UINT, 'required' => true, 'autoIncrement' => true],
+                'post_id' => ['type' => self::UINT, 'required' => true],
+                'user_id' => ['type' => self::UINT, 'required' => true],
+                'reputation' => ['type' => self::INT, 'required' => true],
+                'comment' => ['type' => self::STR, 'required' => true, 'maxLength' => 255],
+                'date' => ['type' => self::UINT, 'default' => time()]
+            ];
+        $structure->getters = [];
+        $structure->relations = 
+            [
+                'Post' => 
+                    [
+                        'entity' => 'XF:Post',
+                        'type' => self::TO_ONE,
+                        'conditions' => 'post_id',
+                        'primary' => true
+                    ],
+            ];
+    
+        return $structure;
+    }
+}
