8484d2812fc83971ffd0c27d2526ef657fca6e44
Addy <aclaure@gmail.com>
2020-11-09 10:31:50 -0500

    removing junk;

--- Listener.php
+++ Listener.php
@@ -12,50 +12,7 @@ class Listener
 {
     public static function postEntityStructure(\XF\Mvc\Entity\Manager $em, \XF\Mvc\Entity\Structure &$structure)
     {
-        // $structure->relations['Reputation'] = 
-        //     [
-        //         'entity' => 'lulzapps\Rep:Reputation',
-        //         'type' => Entity::TO_MANY,
-        //         'conditions' => [['post_id', '=', '$post_id']],
-        //         'primary' => true
-        //     ];
-
-        // print('<pre>');
-        // print_r($structure);
-        // print('</pre>');
-        // print('<hr/>');
-
-        // print('<pre>');
-        // print_r($em);
-        // print('</pre>');
-        // print('<hr/>');
-
-        // $finder = \XF::finder('lulzapps\Rep:Reputation');
-        // $reputations = $finder
-
-
-        // $structure->columns['reputation_list'] = 
-        //     [
-        //         'type' => Entity::BOOL, 
-        //         'default' => false
-        //     ];
-
-        // $structure->relations = 
-        //     [
-        //         'Reputation' => 
-        //             [
-        //                 'entity' => 'lulzapps\Rep:Reputation',
-        //                 'type' => Entity::TO_MANY,
-        //                 'conditions' => 
-        //                     [
-        //                         ['post_id', '=', '$post_id']
-        //                     ],
-        //                 'primary' => true
-        //             ],
-        //     ];
-
-
-        $structure->columns["lulzapps_foo"] = 
+        $structure->columns['lulzapps_foo'] = 
             [
                 'type' => Entity::UINT, 
                 'default' => 42
@@ -68,9 +25,5 @@ class Listener
                 'conditions' => 'post_id',
                 'primary' => true
             ];
-
-        // print('<pre>');
-        // print_r($structure);
-        // print('</pre>');
     }
 }
