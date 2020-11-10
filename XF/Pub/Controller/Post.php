c3d3447465b1ca363ade64852488c52f76d726e7
Addy <aclaure@gmail.com>
2020-11-10 06:27:07 -0500

    ui confirms a save, but doesn’t actually save;

--- XF/Pub/Controller/Post.php
+++ XF/Pub/Controller/Post.php
@@ -6,47 +6,6 @@ use XF\Mvc\ParameterBag;
 
 class Post extends XFCP_Post
 {
-	// public function actionPreview(ParameterBag $params)
-	// {
-    // }
-
-    // protected function assertViewablePost($postId, array $extraWith = [])
-	// {
-    // }
-
-	// public function actionIndex(ParameterBag $params)
-	// {
-    //     $post = $this->assertViewablePost($params->post_id);
-    //     print('<pre>');
-    //     print_r($post);
-    //     print('</pre>');
-
-	// 	return $this->redirectPermanently($this->plugin('XF:Thread')->getPostLink($post));
-	// }
-
-	// public function actionShow(ParameterBag $params)
-	// {
-    //     $post = $this->assertViewablePost($params->post_id);
-    //     print('<pre>');
-    //     print_r($post); 
-    //     print('</pre>');
-
-	// 	$viewParams = [
-	// 		'post' => $post,
-	// 		'thread' => $post->Thread,
-	// 		'forum' => $post->Thread->Forum,
-	// 		'canInlineMod' => $post->canUseInlineModeration()
-	// 	];
-	// 	return $this->view('XF:Post\Show', 'post', $viewParams);
-    // }
-    
-    // public function actionEdit(ParameterBag $params)
-    // {
-    //     $reply = parent::actionEdit($params);
-    //     echo print_r($reply);
-    //     return $reply;
-    // }
-
     public function actionHelloWorld()
     {
         return $this->message('Hello world!111');
@@ -54,40 +13,20 @@ class Post extends XFCP_Post
 
     public function actionReputation(ParameterBag $params) 
     {
-        // // /** @var \XF\ControllerPlugin\Report $reportPlugin */
-        // // $reputationPlugin = $this->plugin('XF:Report');
-        
-        $viewParams = 
-        [
-            'post_id' => $params->post_id,
-        ];
+        $post = $this->assertViewablePost($params->post_id);
+
+        $confirmUrl = $this->buildLink('posts/reputation', $post);
+        $returnUrl = $this->buildLink('posts', $post);
 
         if ($this->request->isPost())
         {
-            return $this->redirect("", \XF::phrase('thank_you_for_reporting_this_content'));
-        //     return $this->view('lulzapps:RepView', 'lulzapps_reputation_submit_overlay2', $viewParams);
+            return $this->redirect($returnUrl, \XF::phrase('thank_you_for_reporting_this_content'));
         }
-        
-        print('<pre>');
-        print_r($params);
-        print('</pre>');
-        return $this->view('lulzapps:RepView', 'lulzapps_reputation_submit_overlay', $viewParams);
+
+        $viewParams = [
+            'confirmUrl' => $confirmUrl,
+            'content' => $post
+        ];
+        return $this->view('XF:Report\RepView', 'lulzapps_reputation_submit_overlay', $viewParams);
     }
-
-    // public function actionReport(ParameterBag $params)
-	// {
-	// 	$profilePost = $this->assertViewableProfilePost($params->profile_post_id);
-	// 	if (!$profilePost->canReport($error))
-	// 	{
-	// 		return $this->noPermission($error);
-	// 	}
-
-	// 	/** @var \XF\ControllerPlugin\Report $reportPlugin */
-	// 	$reportPlugin = $this->plugin('XF:Report');
-	// 	return $reportPlugin->actionReport(
-	// 		'profile_post', $profilePost,
-	// 		$this->buildLink('profile-posts/report', $profilePost),
-	// 		$this->buildLink('profile-posts', $profilePost)
-	// 	);
-	// }
 }
